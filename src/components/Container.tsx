359a65727e40e662999eede2fc812d59ac9f568f
Gidraf Orenja <gidraforenja@outlook.com>
2022-07-25 17:15:08 +0300

    Impliment view films details

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,49 +1,13 @@
 import React, { useEffect, useState, ComponentType } from 'react';
 import styled from 'styled-components';
-import searchFilm, { animationsVideos } from '../actions/films.actions';
 import StarWarsMask from '../assets/images/starwars.jpeg';
-import { PageTransition } from '@steveeeie/react-page-transition';
 import { BrowserRouter, Routes, Route, Link, Router } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../reducers/hooks';
 import SearchComponent from './SearchComponent';
 import FilmDetails from './FilmDetail';
-
-const StyledContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  float: left;
-  position: relative;
-  background: rgb(44 44 44);
-`;
-
-const MaskeImage = styled.img`
-  width: 80%;
-  top: 1rem;
-  right: 3rem;
-  opacity: 0.1;
-  border: none;
-  border-radius: 30px;
-  box-shadow: 0 0 10px rgb(44 44 44) inset;
-  position: absolute;
-  z-index: 0;
-`;
+import { MaskeImage, StyledContainer } from './Components';
 
 const Container = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [value, setValue] = useState('');
-  const dispatch = useAppDispatch();
-  const films = useAppSelector((state: any) => {
-    console.log(state);
-    return state.films;
-  });
-
-  useEffect(() => {
-    dispatch(animationsVideos());
-  }, []);
-
-  const toggleIsLoading = () => {
-    setIsLoading(!isLoading);
-  };
   return (
     <StyledContainer>
       <MaskeImage src={StarWarsMask} />
--- src/reducers/AllReducer.tsx
+++ src/reducers/AllReducer.tsx
@@ -3,19 +3,15 @@ import { act } from 'react-dom/test-utils';
 import Constants from '../utils/Constants';
 import type { RootState } from './index';
 
-interface FilmsState {
-  results: [];
-}
-
-const initialState: FilmsState = {
-  results: [],
-};
-
 const filmsSlice = createSlice({
   name: 'films',
   initialState: [],
   reducers: {
     searchReducer: {
+      reducer: (state, action: PayloadAction<[]>) => (state = action.payload),
+      prepare: (value?: []) => ({ payload: value || [] }),
+    },
+    animationReducer: {
       reducer: (state, action: PayloadAction<[]>) => (state = action.payload),
       prepare: (value?: []) => ({ payload: value || [] }),
     },
